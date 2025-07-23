f8168a4b01a5a27ac60fb66fca76e7814bda3996
te220618 <tejc220618@gmail.com>
2025-07-23 11:43:58 +0900

    aws

--- src/test/java/com/example/equipmentmanagement/EquipmentmanagementApplicationTests.java
+++ src/test/java/com/example/equipmentmanagement/EquipmentmanagementApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.equipmentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EquipmentmanagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+@SpringBootTest(properties = {
+	"spring.datasource.url=jdbc:mysql://localhost:3306/equipmentdb2?useSSL=false&serverTimezone=Asia/Tokyo",
+	"spring.datasource.username=teamkaihatsu",
+	"spring.datasource.password=password",
+	"spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver",
+	"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect"
+  })
+  class EquipmentmanagementApplicationTests {
+	  @Test
+	  void contextLoads() {
+	  }
+  }
+  
